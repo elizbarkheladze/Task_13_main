154661a39263f6f45b84872c1031505a23312746
elizbar <elizbar>
2022-07-04 21:01:11 +0400

    Initial Commit

---
+++ lesson_13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson_13
+//
+//  Created by alta on 7/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
