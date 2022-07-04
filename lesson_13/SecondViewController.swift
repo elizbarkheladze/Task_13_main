02a305d8e708b7ac06275c1967a3d788de87913a
elizbar <elizbar>
2022-07-05 01:37:16 +0400

    fisrt commit

---
+++ lesson_13/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  lesson_13
+//
+//  Created by alta on 7/5/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet weak var save_button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        save_button.layer.cornerRadius = 30
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- lesson_13/ViewController.swift
+++ lesson_13/ViewController.swift
@@ -9,8 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var log_But: UIButton!
+    
+    @IBOutlet weak var sign_But: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        log_But.layer.cornerRadius = 30
+        sign_But.layer.cornerRadius = 30
         // Do any additional setup after loading the view.
     }
 
